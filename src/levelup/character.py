3a58b4cdfae746238254642ce733aef63eec9607
Suresh <surenat@gmail.com>
2023-10-26 17:20:40 -0400

    character new methods

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -2,12 +2,24 @@ class Character:
 
     name = ""
     position=0
+    gamemap=""
 
     def __init__(self, character_name):
         self.name = character_name
     
+    def getName(self):
+        return self.getName()
+
+    def getPosition(self):
+        return self.position
+        
     def setPosition(self, position):
         self.position = position1
         
+    def enterMap(self, gamemap):
+        self.gamemap = gamemap
+
+    def move(self, direction):
+        self.gamemap.calculatePosition(position, direction)
     
     
