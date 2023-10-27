ecfc4facb192b6b811f98ab1f91335f21d8636d8
Hussain Fadwani <hussainalifadwani@gmail.com>
2023-10-27 12:52:02 -0400

    updated controller and added fake character

--- src/levelup/controller.py
+++ src/levelup/controller.py
@@ -1,25 +1,19 @@
-import logging
-from dataclasses import dataclass
-from enum import Enum
 from levelup.character import Character
-
+from levelup.direction import Direction
+from levelup.gamemap import GameMap
+from levelup.position import Position
 
 DEFAULT_CHARACTER_NAME = "Character"
 
-#TODO: ADD THINGS YOU NEED FOR STATUS
-@dataclass
+
 class GameStatus:
     running: bool = False
     character_name: str = DEFAULT_CHARACTER_NAME
-    # NOTE - Game status will have this as a tuple. The Position should probably be in a class
     current_position: tuple = (-100,-100)
     move_count: int = 0
 
-class Direction(Enum):
-    NORTH = "n"
-    SOUTH = "s"
-    EAST = "e"
-    WEST = "w"
+    def __str__(self) -> str:
+        return f"{self.character_name} is currently on {self.current_position} and moved {self.move_count} times."
 
 class CharacterNotFoundException(Exception):
     pass
@@ -28,31 +22,34 @@ class InvalidMoveException(Exception):
     pass
 
 class GameController:
-
-
     status: GameStatus
     character: Character
+    map: GameMap
 
     def __init__(self):
         self.status = GameStatus()
 
     def start_game(self):
-        pass
+        self.map = GameMap()
+        if self.character == None:
+            self.create_character(DEFAULT_CHARACTER_NAME)
+        self.character.enter_map(self.map)
+        self.status.running = True
+        self.status.current_position = (self.character.current_position.x, self.character.current_position.y)
+        self.status.move_count = 0
 
-    # Pre-implemented to demonstrate ATDD
     def create_character(self, character_name: str) -> None:
-        if character_name is None or character_name == "":
-            self.status.character_name = DEFAULT_CHARACTER_NAME
+        if character_name is not None and character_name != "":
+            self.character = Character(character_name)
         else:
-            self.status.character_name = character_name
-        self.status.character_name = self.character.character_name
+            self.character = Character(DEFAULT_CHARACTER_NAME)
+        self.status.character_name = self.character.name
 
     def move(self, direction: Direction) -> None:
         self.character.move(direction)
         self.status.current_position = (self.character.current_position.x, self.character.current_position.y)
         self.status.move_count = self.status.move_count + 1
-        status.movecount
-   
+
     def set_character_position(self, xycoordinates: tuple) -> None:
         x = xycoordinates[0]
         y = xycoordinates[1]
@@ -60,13 +57,13 @@ class GameController:
         self.status.current_position = xycoordinates
 
     def set_current_move_count(self, move_count: int) -> None:
-        # IMPLEMENT THIS TO SET CURRENT MOVE COUNT -- exists to be testable
         self.status.move_count = move_count
-        
+
     def get_total_positions(self) -> int:
-        # IMPLEMENT THIS TO GET THE TOTAL POSITIONS FROM THE MAP - - exists to be
-        # testable
-        return self.character.gamemap.getTotalPositions()
-        
+        return self.map.size[0]*self.map.size[1]
+    
+    def initalize_game_for_testing(self) -> None:
+        self.create_character("")
+        self.start_game()
 
     
--- tests/test_game_controller.py
+++ tests/test_game_controller.py
@@ -1,11 +1,43 @@
 from unittest import TestCase
 from levelup.controller import GameController
+from fake_character import FakeCharacter
+from levelup.direction import Direction
 
-# THIS IS AN EXAMPLE UNIT TEST. 
-# All the unit tests for the Game Controller should go here
-# Unit tests for other classes should be in their own .py files (like test_character.py)
 class TestGameController(TestCase):
     def test_init(self):
-        testObj = GameController()
-        assert testObj.status != None
+        testobj = GameController()
+        assert testobj.status != None
+
+    def test_create_character_updates_status(self):
+        testobj = GameController()
+        arbitrary_name = "ARBITRARY"
+        testobj.create_character(arbitrary_name)
+        self.assertEqual(arbitrary_name, testobj.status.character_name)
+        self.assertIsNotNone(testobj.character)
+
+    def test_start_game_creates_map_and_enters_char(self):
+        testobj = GameController()
+        arbitrary_name = "ARBITRARY"
+        fake_char = FakeCharacter(arbitrary_name)
+        testobj.character = fake_char
+
+        testobj.start_game()
+        
+        self.assertIsNotNone(testobj.map)
+        self.assertTrue(fake_char.is_enter_map_called)
+        self.assertTrue(testobj.status.running)
+        self.assertEqual(0, testobj.status.move_count)
+
+    def test_move_calls_char_move(self):
+        testobj = GameController()
+        arbitrary_name = "ARBITRARY"
+        fake_char = FakeCharacter(arbitrary_name)
+        testobj.character = fake_char
+        arbitrary_direction = Direction.NORTH
+
+        testobj.move(arbitrary_direction)
+
+        self.assertTrue(fake_char.is_move_called)
+        self.assertEqual(fake_char.last_move_direction, arbitrary_direction)
+        
         
